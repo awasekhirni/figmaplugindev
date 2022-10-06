b820e7e0a7541f9cac7c695651b70df61e6fdafd
awasekhirni@gmail.com <awasekhirni@gmail.com>
2022-10-06 16:20:15 -0400

    zeroplugin figma plugin with es6
    figma plugin basic with es6

---
+++ zeroplugin/code.js
@@ -0,0 +1,24 @@
+//Awase Khirni Syed 
+// Elain Technologies Inc,Canada 2020-2021
+
+//my first plugin development for figma
+
+
+function runPlugin(){
+
+    //get the number of selected elements 
+    let selectedElements = figma.currentPage.selection
+
+    console.log(selectedElements)
+
+    let noOfselectedElements=figma.currentPage.selection.length
+    console.log(noOfselectedElements)
+
+    let selectedName=figma.currentPage.selection[0].name
+    console.log('selectedName:'+selectedName)
+
+    //close plugin
+    figma.closePlugin()
+}
+
+
